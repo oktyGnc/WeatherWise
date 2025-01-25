cdafbdaada55d0392e6a9954426c32381822519b
Oktay Genç <gncoktay3866@gmail.com>
2025-01-25 23:21:56 +0300

    Fix: ErrorCard message change it

--- app/src/main/java/com/oktaygenc/weatherwise/ui/presentation/home/components/ErrorCard.kt
+++ app/src/main/java/com/oktaygenc/weatherwise/ui/presentation/home/components/ErrorCard.kt
@@ -7,17 +7,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun ErrorCard(message: String) {
     Card(
         colors = CardDefaults.cardColors(
-            containerColor = Color.Red.copy(alpha = 0.3f)
+            containerColor = Color.Red.copy(0.6f)
         ), modifier = Modifier.padding(16.dp)
     ) {
         Text(
-            text = message, color = Color.White, modifier = Modifier.padding(16.dp)
+            text = message,
+            color = Color.White,
+            fontWeight = FontWeight.ExtraBold,
+            modifier = Modifier.padding(16.dp)
         )
     }
 }
